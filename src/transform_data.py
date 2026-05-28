069d8b53c0f4feb96a7f8e7a6c9a31b6688b2de7
Jothan <jothanalmeida7@gmail.com>
2026-05-28 18:29:47 -0300

    feat: adiciona função de normalização da coluna weather

--- src/transform_data.py
+++ src/transform_data.py
@@ -25,3 +25,24 @@ def create_dataframe(path_name: str) -> pd.DataFrame:
     logging.INFO(f"-> Data frame criado com {len(df)} linha(s)\n")
 
     return df
+
+def normalize_dataframe(df: pd.DataFrame)->pd.DataFrame:
+    logging.INFO("-> Normalizando o data frame...\n")
+
+    # lambda transforma a lista contendo um dicionário em colunas
+    df_weather = pd.json_normalize(df["weather"].apply(lambda x: x[0]))
+
+    # Renomear colunas pois nomes já existem
+    df_weather = df_weather.rename(columns={
+        "id": "weather_id",
+        "main": "weather_main",
+        "description": "weather_description",
+        "icon": "weather_icon"
+    })
+
+    # Concatena as colunas
+    df = pd.concat([df, df_weather], axis=1)
+
+    logging.INFO(f"Coluna 'weather' normalizada - {len(df.columns)} colunas")
+
+    return df
